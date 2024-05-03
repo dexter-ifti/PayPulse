eea348f04f88df02833da2bfdc4f1e6f5099920e
dexter-ifti <tiftikhar@student.iul.ac.in>
2024-05-03 23:56:56 +0530

    feat: Add routing and pages for SignUp, SignIn, Dashboard, and SendMoney
    This commit adds routing and pages for SignUp, SignIn, Dashboard, and SendMoney components to the frontend. The SignUp and SignIn pages allow users to create a new account and sign in to their accounts, respectively. The Dashboard page displays the user's balance and a list of users, while the SendMoney page allows users to send money to their friends. These changes are part of the ongoing development of the authentication and transaction flow in the application.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,20 @@
-
+import React from 'react'
+import { SignUp, SignIn, Dashboard, SendMoney } from './pages'
+import { Heading } from './components'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 function App() {
 
   return (
-    <div>
-        Hello world
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/signup' element={<SignUp />} />
+          <Route path='signin' element={<SignIn />} />
+          <Route path='/dashboard' element={<Dashboard />} />
+          <Route path='send' element={<SendMoney />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   )
 }
 
