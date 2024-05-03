d36799af7a028022943b665469bb42e9105376e8
dexter-ifti <tiftikhar@student.iul.ac.in>
2024-05-03 23:56:00 +0530

    feat: Add components for Heading, SubHeading, Button, InputBox, Appbar, Balance, BottomWarning, and Users
    This commit adds the following components to the frontend: Heading, SubHeading, Button, InputBox, Appbar, Balance, BottomWarning, and Users. These components provide various UI elements and functionality for the application.

---
+++ frontend/src/components/index.js
@@ -0,0 +1,19 @@
+import Heading from "./Heading";
+import SubHeading from "./SubHeading";
+import Button from "./Button";
+import InputBox from "./InputBox";
+import Appbar from "./Appbar";
+import Balance from "./Balance";
+import BottomWarning from "./BottomWarning";
+import Users from "./Users";
+
+export {
+    Heading,
+    SubHeading,
+    Button,
+    InputBox,
+    Appbar,
+    Balance,
+    BottomWarning,
+    Users
+    }
