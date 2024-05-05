80b813bb93d846b33964ee828cab0218763f52e4
dexter-ifti <tiftikhar@student.iul.ac.in>
2024-05-05 07:06:25 +0530

    feat: Fix axios URL encoding issue in Users component
    The axios URL in the Users component had an issue with URL encoding the `filter` parameter. This commit fixes the issue by properly encoding the `filter` parameter using template literals.

--- frontend/src/components/Users.jsx
+++ frontend/src/components/Users.jsx
@@ -8,7 +8,7 @@ function Users() {
   const [filter, setFilter] = useState('');
 
   useEffect(() => {
-    axios.get(`http://localhost:3000/api/v1/user/bulk?filter={filter}`)
+    axios.get(`http://localhost:3000/api/v1/user/bulk?filter=${filter}`)
     .then(response => setUsers(response.data.user))
   }, [filter])
   return (
