cc3a457f19d35e43dedbf567dba81c0ff9ee037c
Taha Iftikhar <tiftikhar@student.iul.ac.in>
2024-07-08 23:12:38 +0530

    feat: Update Home page layout and styling
    This commit updates the layout and styling of the Home page component. It improves the visual appearance of the page by using a different background color and adjusting the font sizes for better readability. Additionally, it refactors the code to use CSS grid for better responsiveness and adds a grid gap between the two sections. These changes enhance the overall user experience and make the Home page more visually appealing.

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,23 +1,31 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Home() {
     return (
-        <div className="min-h-screen flex flex-col items-center justify-center light:bg-gray-100 p-4">
-            <div className="bg-white shadow-md rounded-lg p-6 mb-6 w-full max-w-md">
-                <h1 className="text-2xl font-bold mb-4 text-center text-pink-950">New Here?</h1>
-                <Link to="/SignUp" className="block text-center text-blue-500 hover:underline border-4">
-                    Sign Up
-                </Link>
+        <div className="min-h-screen flex flex-col items-center justify-center bg-black-100 p-4">
+            <div className="text-5xl sm:text-6xl md:text-7xl lg:text-8xl xl:text-9xl font-semibold text-orange-800 mb-6 text-center">
+                PayPulse App
             </div>
-            <div className="bg-white shadow-md rounded-lg p-6 w-full max-w-md">
-                <h1 className="text-2xl font-bold mb-4 text-center text-red-600">Already have an account?</h1>
-                <Link to="/SignIn" className="block text-center text-blue-500 hover:underline border-4">
-                    Sign In
-                </Link>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-10 md:gap-20 w-full max-w-4xl">
+
+                <div className="bg-white shadow-md rounded-lg p-6 mb-6 md:mb-0 w-full max-w-md mx-auto md:mx-0">
+                    <h1 className="text-2xl font-bold mb-4 text-center text-pink-950">New Here?</h1>
+                    <Link to="/SignUp" className="block text-center text-blue-500 hover:underline">
+                        Sign Up
+                    </Link>
+                </div>
+
+                <div className="bg-white shadow-md rounded-lg p-6 w-full max-w-md mx-auto md:mx-0">
+                    <h1 className="text-2xl font-bold mb-4 text-center text-red-600">Already have an account?</h1>
+                    <Link to="/SignIn" className="block text-center text-blue-500 hover:underline">
+                        Sign In
+                    </Link>
+                </div>
+
             </div>
         </div>
-    )
+    );
 }
 
-export default Home
+export default Home;
