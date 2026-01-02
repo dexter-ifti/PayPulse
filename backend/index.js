083bbdec415b3c5897c4f87b2c63a5b5c2ca8125
Taha Iftikhar <tiftikhar@student.iul.ac.in>
2026-01-02 20:02:56 +0530

    feat: allow all CORS origins by removing specific origin configuration

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ const rootRouter = require('./routes/index');
 
 const app = express();
 
-app.use(cors({ origin: 'https://pay-pulse-roan.vercel.app' }));
+app.use(cors());
 app.use(express.json());
 
 const port = process.env.PORT || 4000;
