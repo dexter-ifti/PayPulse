f4e8134c940b4de313d8d3552672894253538f4a
Taha Iftikhar <tiftikhar@student.iul.ac.in>
2026-01-04 03:11:00 +0530

    added new routes

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -6,7 +6,10 @@ const { userController } = require('../controllers');
 // User routes
 router.post('/signup', userController.signup);
 router.post('/signin', userController.signin);
+router.post('/logout', authMiddleware, userController.logoutUser);
+router.post('/refresh-token', authMiddleware, userController.refreshAccessToken);
+router.get('/current-user', authMiddleware, userController.getCurrentUser);
 router.put('/', authMiddleware, userController.updateUser);
-router.get('/bulk', userController.bulkSearch);
+router.get('/bulk', authMiddleware, userController.bulkSearch);
 
 module.exports = router;
