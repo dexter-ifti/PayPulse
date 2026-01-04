1642be2855e524bc9f030725380287e163802236
Taha Iftikhar <tiftikhar@student.iul.ac.in>
2026-01-05 03:47:36 +0530

    feat(auth): Implement JWT-based access/refresh token authentication with password hashing, HTTP-only cookies, and add token refresh and logout endpoints.

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
-const userShcema = mongoose.Schema({
+const userSchema = mongoose.Schema({
     username: {
         type : String,
         required : true,
@@ -26,8 +29,51 @@ const userShcema = mongoose.Schema({
         trim : true,
         minLength : 3
     },
+    refreshToken : {
+        type : String,
+        default : ""
+    }
+}, {
+    timestamps : true
 });
 
-const User = mongoose.model('User', userShcema);
+userSchema.pre('save', async function(){
+    if(!this.isModified('password')) return ;
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+})
+
+userSchema.methods.isPasswordCorrect = async function(password){
+    return await bcrypt.compare(password, this.password);
+}
+
+userSchema.methods.generateAccessToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            firstName: this.firstName,
+            lastName: this.lastName
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+userSchema.methods.generateRefreshToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+const User = mongoose.model('User', userSchema);
 
 module.exports = {User};
