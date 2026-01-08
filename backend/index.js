507d3a982805589ed3a84a3f89e56c864caf6907
Taha Iftikhar <tiftikhar@student.iul.ac.in>
2026-01-08 21:52:38 +0530

    Revert "reverting changes for cors config"
    This reverts commit 575a973a7072e1d32166eecb6ea88266a41cbb11.

--- backend/index.js
+++ backend/index.js
@@ -8,26 +8,9 @@ const rootRouter = require("./routes/index");
 
 const app = express();
 
-const allowedOrigins = [
-  "http://localhost:5173",
-  "https://pay-pulse-roan.vercel.app"
-];
 
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin) return callback(null, true); // allow non-browser tools
-    if (allowedOrigins.includes(origin)) {
-      return callback(null, true);
-    }
-    return callback(new Error("CORS not allowed"));
-  },
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: false
-}));
+app.use(cors());
 
-// 👇 THIS LINE IS CRITICAL
-app.options("*", cors());
 
 app.use(express.json());
 
