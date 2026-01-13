0fedea8369f663b7c2391bb6adc40f9bcbddf6af
Taha Iftikhar <tiftikhar@student.iul.ac.in>
2026-01-14 00:17:24 +0530

    made the bulk route protected

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -10,6 +10,6 @@ router.post('/logout', authMiddleware, userController.logoutUser);
 router.post('/refresh-token', authMiddleware, userController.refreshAccessToken);
 router.get('/current-user', authMiddleware, userController.getCurrentUser);
 router.put('/', authMiddleware, userController.updateUser);
-router.get('/bulk', userController.bulkSearch);
+router.get('/bulk', authMiddleware, userController.bulkSearch);
 
 module.exports = router;
