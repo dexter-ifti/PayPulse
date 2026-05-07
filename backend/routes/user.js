026c7bd14355b9e1af17df663f9cf52a88c19e20
Taha Iftikhar <tiftikhar@student.iul.ac.in>
2026-05-07 11:00:00 +0530

    added turnstile middleware in routes

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { authMiddleware } = require('../middlewares');
+const { authMiddleware, turnstileMiddleware } = require('../middlewares');
 const { userController } = require('../controllers');
 
 // User routes
-router.post('/signup', userController.signup);
-router.post('/signin', userController.signin);
+router.post('/signup', turnstileMiddleware, userController.signup);
+router.post('/signin', turnstileMiddleware, userController.signin);
 router.post('/logout', authMiddleware, userController.logoutUser);
 router.post('/refresh-token', authMiddleware, userController.refreshAccessToken);
 router.get('/current-user', authMiddleware, userController.getCurrentUser);
