ad5d744080255672ceda68ab3fff910d54430543
Taha Iftikhar <tiftikhar@student.iul.ac.in>
2026-07-26 18:01:45 +0530

    enhance account routes: add ledger history endpoint for transaction tracking

--- backend/routes/account.js
+++ backend/routes/account.js
@@ -7,5 +7,6 @@ const { accountController } = require('../controllers');
 router.get('/balance', authMiddleware, accountController.getBalance);
 router.post('/transfer', authMiddleware, accountController.transfer);
 router.get('/transactions', authMiddleware, accountController.getTransactionHistory);
+router.get('/ledger', authMiddleware, accountController.getLedgerHistory);
 
 module.exports = router;
